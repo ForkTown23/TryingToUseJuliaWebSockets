e33876a120fe801583f5e83b6cf5b86454d4a10d
= <=>
2023-01-25 14:16:26 -0800

    filled out the rest of the sanittizer functions

--- institutional_server.jl
+++ institutional_server.jl
@@ -36,10 +36,9 @@ function sanitize_add_course(param_string::Vector{SubString{String}})
 end
 
 function sanitize_add_prereq(param_string::Vector{SubString{String}})
-    #clean_params = param_string
     # there are supposed to be two entries here.
     if length(param_string) != 2
-        throw(ArgumentError("There's too many courses here, we just need two"))
+        throw(ArgumentError("There's a weird number of courses here, we just need two."))
     end
     # they are in the format: "Target-Name=COURSE+CODE&Prereq-Name=COURSE+CODE"
     clean_params = Vector{String}()
@@ -52,14 +51,33 @@ function sanitize_add_prereq(param_string::Vector{SubString{String}})
 end
 
 function sanitize_remove_course(param_string::Vector{SubString{String}})
-    clean_params = param_string
-    # TODO
+    # there is supposed to be one entry here
+    if length(param_string) != 1
+        throw(ArgumentError("There's a weird number of courses here, we just need one."))
+    end
+    # it should be in the format "Target-Name=COURSE+CODE"
+    clean_params = Vector{String}()
+    for pair in param_string
+        course_w_code = split(pair, "=")[2]
+        course_w_code = replace(course_w_code, "+" => " ")
+        push!(clean_params, course_w_code)
+    end
     return clean_params
 end
 
 function sanitize_remove_prereq(param_string::Vector{SubString{String}})
-    clean_params = param_string
-    # TODO
+    # there are supposed to be two entries here.
+    if length(param_string) != 2
+        throw(ArgumentError("There's a weird number of courses here, we just need two."))
+    end
+    # they are in the format: "Target-Name=COURSE+CODE&Prereq-Name=COURSE+CODE"
+    clean_params = Vector{String}()
+    for pair in param_string
+        course_w_code = split(pair, "=")[2]
+        course_w_code = replace(course_w_code, "+" => " ")
+        push!(clean_params, course_w_code)
+    end
+    return clean_params
     return clean_params
 end
 
@@ -98,14 +116,19 @@ server = HTTP.serve() do request::HTTP.Request
             response = "Alright! Let's remove a course!"
             # sanitize for remove prereq
             clean_params = sanitize_remove_course(request_strings[2:end])
-            # then call it TODO
-            affected = ""
+            # then call it
+            affected = delete_course_institutional(clean_params[1], big_curric)
+            # collect the plans and print properly
+            (affected, count) = print_affected_plans_web(affected)
+            affected = affected * "Number of plans affected $count"
         elseif (method == "remove-prereq")
             response = "Alright! Let's remove a prereq!"
             #sanitize for remove prereq
             clean_params = sanitize_remove_prereq(request_strings[2:end])
-            # then call it TODO
-            affected = ""
+            # then call it
+            affected = delete_prerequisite_institutional(clean_params[1], clean_params[2], big_curric)
+            (affected, count) = print_affected_plans_web(affected)
+            affected = affected * "Number of plans affected $count"
         else
             throw(ArgumentError("Hey, I'm not sure what method you're trying to call. Please try again :)"))
         end
