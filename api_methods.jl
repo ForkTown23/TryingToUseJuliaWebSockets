4b1420f1c6150af5c422f5dd1c1881ca83df2593
= <=>
2023-02-06 12:04:33 -0800

    api methods file with more detailed output

---
+++ api_methods.jl
@@ -0,0 +1,147 @@
+using CurricularAnalytics, CurricularAnalyticsDiff
+
+# 1) find course in condensed and the prereq (ignore this one)
+# 2) get list of plans from the what if institutional
+# 3) for each plan:
+## read the plan csv
+## get curric object from it
+## remove the prereq
+## run diff (maybe not)
+## record complexity & unit score differences
+function add_prereq_inst_web(course_name::AbstractString, prereq::AbstractString)
+    try
+        results = Dict()
+        # 0) read the condensed
+        condensed = read_csv("./files/condensed2.csv")
+        # 2) get the list of affected plans
+        affected = add_prereq_institutional(condensed, course_name, prereq)
+        # 3) for each affected plan:
+        plans = filter(x -> x != "", affected)
+        for plan in plans
+            major = plan[1:4]
+            college = plan[5:end]
+            curr = read_csv("./files/output/$(major)/$(college).csv")
+            if typeof(curr) == DegreePlan
+                curr = curr.curriculum
+            end
+            try
+                results[major]
+            catch
+                results[major] = Dict()
+            end
+            ## add the prereq
+            ## this is harder than the initial version
+            ## have to go through each prereq of the prereq you want to add
+            ## and add that to the new curriculum 
+            try 
+                new_curr = add_prereq(course_name, prereq, curr, pre)
+            catch
+                
+            ## don't run diff, just check the total credit hours and complexity scores 
+            ch_diff = new_curr.credit_hours - curr.credit_hours
+            complex_diff = complexity(new_curr)[1] - complexity(curr)[1] # consider using complexity(curr)
+            # write the results in 
+            results[major][college] = Dict()
+            results[major][college]["complexity"] = complex_diff
+            results[major][college]["unit change"] = ch_diff
+        end
+    catch e
+        throw(e)
+    end
+end
+
+# 1) find course in condensed and the prereq (ignore this one)
+# 2) get list of plans from the what if institutional
+# 3) for each plan:
+## read the plan csv
+## get curric object from it
+## remove the prereq
+## run diff (maybe not)
+## record complexity & unit score differences
+function remove_prereq_inst_web(target_name::AbstractString, prereq_name::AbstractString)
+    try
+        results = Dict()
+        # 0) read the condensed 
+        condensed = read_csv("./files/condensed2.csv")
+        # 2) get the list of plans
+        affected = delete_prerequisite_institutional(target_name, prereq_name, condensed)
+        # 3) for each plan
+        plans = filter(x -> x != "", affected)
+        println(affected)
+        for plan in plans
+            major = plan[1:4]
+            college = plan[5:end]
+            curr = read_csv("./files/output/$(major)/$(college).csv")
+            if typeof(curr) == DegreePlan
+                curr = curr.curriculum
+            end
+            try
+                results[major]
+            catch
+                results[major] = Dict()
+            end
+            ## remove the prereq
+            new_curr = remove_prereq(target_name, prereq_name, curr)
+            ## don't run diff, just check the total credit hours and complexity scores 
+            ch_diff = new_curr.credit_hours - curr.credit_hours
+            complex_diff = complexity(new_curr)[1] - complexity(curr)[1] # consider using complexity(curr)
+            # write the results in 
+            results[major][college] = Dict()
+            results[major][college]["complexity"] = complex_diff
+            results[major][college]["unit change"] = ch_diff
+        end
+        return results
+    catch e
+        throw(e)
+    end
+end
+# 1) find course in condensed 
+# 2) get list of plans from the canonical Name
+# 3) for each plan:
+## read the plan csv
+## get curric object from it
+## delete the course
+## run diff (maybe not)
+## record complexity & unit score differences
+function remove_course_inst_web(course_name::AbstractString)
+    try
+        results = Dict()
+        # 0) read the condensed
+        condensed = read_csv("./files/condensed2.csv")
+        # 1)
+        course = course_from_name(course_name, condensed)
+        # 2) get the list of plans from the condensed course
+        plans = filter(x -> x != "", split(course.canonical_name, ","))
+        println(plans)
+        # 3) for each plan
+        for plan in plans
+            println(plan)
+            ## read the plan csv
+            # this is weird and hardcoded, but it should work
+            major = plan[1:4]
+            college = plan[5:end]
+            curr = read_csv("./files/output/$(major)/$(college).csv")
+            if typeof(curr) == DegreePlan
+                curr = curr.curriculum
+            end
+            try
+                results[major]
+            catch
+                results[major] = Dict()
+            end
+            ## delete the course from it
+            new_curr = remove_course(course_name, curr)
+            ## don't run diff, just check the total credit hours and complexity scores 
+            ch_diff = new_curr.credit_hours - curr.credit_hours
+            complex_diff = complexity(new_curr)[1] - complexity(curr)[1] # consider using complexity(curr)
+            # write the results in 
+            results[major][college] = Dict()
+            results[major][college]["complexity"] = complex_diff
+            results[major][college]["unit change"] = ch_diff
+        end
+        return results
+    catch e
+        # dumb, I know
+        throw(e)
+    end
+end
